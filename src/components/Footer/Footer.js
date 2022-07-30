074e0cb2dd94abae0c24f7fcd7d699ce42b5447f
jeo19 <terrakkk@outlook.com>
2022-07-30 15:21:56 +0800

    chore:Add the Footer template

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name="email" type="email" placeholder="Your Email" />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
