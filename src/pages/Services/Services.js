3d5f4532cd649e41d3ade4ca75fb878c64181a04
jeo19 <terrakkk@outlook.com>
2022-08-18 21:11:33 +0800

    feat:Add the components and data object of Services pages

---
+++ src/pages/Services/Services.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { InfoSection, Pricing } from '../../components';
+import { homeObjOne, homeObjThree } from './Data';
+
+function Services() {
+  return (
+    <>
+      <Pricing />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+}
+
+export default Services;import React from 'react';
+import { InfoSection, Pricing } from '../../components';
+import { homeObjOne, homeObjThree } from './Data';
+
+function Services() {
+  return (
+    <>
+      <Pricing />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+}
+
+export default Services;
