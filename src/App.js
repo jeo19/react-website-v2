bac1f532afb610ee0013da40f68a8e8b1e836ba3
jeo19 <terrakkk@outlook.com>
2022-08-24 10:54:54 +0800

    feat:Add the SignUp page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
 import Services from "./pages/Services/Services";
 import Products from "./pages/Products/Products";
+import SignUp from "./pages/SignUp/SignUp";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/services" element={<Services />}></Route>
         <Route path="/products" element={<Products />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
       </Routes>
       <Footer />
     </Router>
